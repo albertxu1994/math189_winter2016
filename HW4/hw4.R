64fef6a4e88c251872696933d6e1e447eddd7e4f
sandrahui <ms.sandra.hui@gmail.com>
2016-03-10 19:12:27 -0800

    fixed prediction intervals

--- HW4/hw4.R
+++ HW4/hw4.R
@@ -1,3 +1,12 @@
+# Math 189, Winter 16, Hw 4
+
+# Sherry Diep, sadiep@ucsd.edu, A10598577
+# Sandra Hui, s3hui@ucsd.edu, A10551236
+# David Lee, dal002@ucsd.edu, A11304749
+# Irving Valles, ivalles@ucsd.edu, A09669593
+# Albert Xu, a8xu@uscd.edu, A10670971
+# Mark Yee, mjyee@ucsd.edu, A11289551
+
 library(ggplot2)
 gauge <- read.table("gauge.txt", header=T)
 
@@ -7,27 +16,6 @@ avgGauge$logGain <- log(avgGauge$gain)
 linearModel <- lm(logGain ~ density, data=avgGauge)
 linModPredict <- predict(linearModel, interval="prediction", se.fit=T, level=0.95)
 avgGauge <- data.frame(avgGauge, linModPredict$fit)
-
-# extract values from the linear model
-yIntercept <- summary(linearModel)$coefficients[1,1]
-yInterceptSE <- summary(linearModel)$coefficients[1,2]
-slope <- summary(linearModel)$coefficients[2,1]
-slopeSE <- summary(linearModel)$coefficients[2,2]
-df <- summary(linearModel)$df[2]
-
-# function to make a point estimate of density and interval around it, given gain
-# log(gain) = slope * density + yIntercept
-predictDensity <- function(linMod, gain) {
-  yIntercept <- summary(linMod)$coefficients[1,1]
-  yInterceptSE <- summary(linMod)$coefficients[1,2]
-  slope <- summary(linMod)$coefficients[2,1]
-  slopeSE <- summary(linMod)$coefficients[2,2]
-  df <- summary(linMod)$df[2]
-  pointEst <- (log(gain) - yIntercept) / slope
-  lowerBound <- (log(gain) - (yIntercept - qt(.975, df=df)*yInterceptSE)) / (slope)
-  upperBound <- (log(gain) - (yIntercept + qt(.975, df=df)*yInterceptSE)) / (slope)
-  list(logInput=log(gain), estimate=pointEst, lowerBound=lowerBound, upperBound=upperBound)
-}
 
 # plot the prediction and confidence intervals
 logGainDensityPlot <- ggplot(avgGauge, aes(x=density, y=logGain)) + 
@@ -57,16 +45,22 @@ qqPlot <- ggplot(linearModel, aes(qqnorm(.stdresid)[[1]], .stdresid)) +
   labs(title="Normal QQ Plot of the Residuals", x="Theoretical Quantiles", y="Standardized Residuals")
 ggsave("qqPlotResiduals.png", plot=qqPlot)
 
+# function to make a point estimate of density and interval around it, given gain
+predictDensity <- function(linMod, gain) {
+  predict(linMod, newdata=gain, level=0.95, interval="prediction")
+}
+
 # predict density for gain reading of 38.6 and 426.7
+pred_linearModel <- lm(density ~ logGain, data=avgGauge)
 print("actual density for avg reading of 38.55:")
 print(avgGauge[avgGauge$gain == 38.55, "density"])
-print("predicted density for avg reading of 38.6:")
-print(predictDensity(linearModel, 38.6))
+print("predicted density for avg reading of 38.55:")
+print(predictDensity(pred_linearModel, data.frame(logGain=log(38.55))))
 
 print("actual density for avg reading of 426.7:")
 print(avgGauge[avgGauge$gain == 426.7, "density"])
 print("predicted density for avg reading of 426.7:")
-print(predictDensity(linearModel, 426.7))
+print(predictDensity(pred_linearModel, data.frame(logGain=log(426.7))))
 
 # cross validation
 # remove densities 0.508 and 0.001, and predict the density for an average reading of 38.6
@@ -74,15 +68,15 @@ not508 <- gauge[gauge$density != 0.508,]
 not508_avgGain <- sapply(unique(not508$density), FUN=function(x) { mean(not508[not508$density == x, "gain"]) })
 not508_avgGauge <- data.frame(density=unique(not508$density), gain=not508_avgGain)
 not508_avgGauge$logGain <- log(not508_avgGauge$gain)
-not508_linearModel <- lm(logGain ~ density, data=not508_avgGauge)
-print("predicted density, removing 0.508, for avg reading of 38.6")
-print(predictDensity(not508_linearModel, 38.6))
+not508_linearModel <- lm(density ~ logGain, data=not508_avgGauge)
+print("predicted density, removing 0.508, for avg reading of 38.55")
+print(predictDensity(not508_linearModel, data.frame(logGain=log(38.55))))
 
 not001 <- gauge[gauge$density != 0.001,]
 not001_avgGain <- sapply(unique(not001$density), FUN=function(x) { mean(not001[not001$density == x, "gain"]) })
 not001_avgGauge <- data.frame(density=unique(not001$density), gain=not001_avgGain)
 not001_avgGauge$logGain <- log(not001_avgGauge$gain)
-not001_linearModel <- lm(logGain ~ density, data=not001_avgGauge)
-print("predicted density, removing 0.001, for avg reading of 38.6")
-print(predictDensity(not001_linearModel, 38.6))
+not001_linearModel <- lm(density ~ logGain, data=not001_avgGauge)
+print("predicted density, removing 0.001, for avg reading of 38.55")
+print(predictDensity(not001_linearModel, data.frame(logGain=log(38.55))))
 
